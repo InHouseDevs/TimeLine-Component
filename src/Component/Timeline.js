a8e479d0b0f61d86f07df6ac96cc6dcfa65ff3b8
Mrityunjay383 <mrityunjayvyasmhl@gmail.com>
2023-02-06 16:26:03 +0530

    Init

---
+++ src/Component/Timeline.js
@@ -0,0 +1,42 @@
+import React, {useEffect, useState} from 'react'
+import "./Timeline.css"
+
+const TimeLine = ({statusIndex, timelinePoints}) => {
+
+    const [gradient, setGradient] = useState({
+        first: 0, last: 0
+    })
+    const updateByState = () => {
+        // const status = "COMPLETED";
+        if (statusIndex === 0) {
+            setGradient({
+                first: 11, last: 1
+            })
+
+        } else if (statusIndex === 1) {
+            setGradient({
+                first: 35, last: 66
+            })
+        } else if (statusIndex === 2) {
+            setGradient({
+                first: 100, last: 2
+            })
+        }
+    }
+
+    useEffect(() => {
+        updateByState();
+    }, [statusIndex])
+
+    return (<div id="timeline">
+        <div className="line"
+             style={{background: `linear-gradient(to right, #5DAC41 ${gradient.first}%, #BFBFBF ${gradient.last}%)`}}>
+            {timelinePoints.map((p, i) => {
+                return <div key={i} data-mobiletext={p}
+                            className={statusIndex >= i ? "year dot complete" : "year dot"}></div>
+            })}
+        </div>
+    </div>)
+}
+
+export default TimeLine;
